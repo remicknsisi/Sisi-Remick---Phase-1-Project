6de2b32ca0db0a19a1fe086644195f77ff9c3a51
Sienna <remicknsisi@gmail.com>
2022-10-29 12:25:29 -0400

    added some styling, new images

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ function handleSubmit(e){
         if(obj.type === input){
             renderClothing.call(obj)
             } 
-            // else alert(`Nothing in stock for ${input}!`)
         })
     )
 }
-
-// document.addEventListener('DOMContentLoadad', renderClothing)
 
 function renderClothing(){
     let clothingContainer = document.getElementById('container')
@@ -27,8 +24,10 @@ function renderClothing(){
             price.innerText = this.price
             let image = document.createElement('img')
             image.src = this.image
+            image.className = "image"
             let addToCart = document.createElement('button')
             addToCart.innerText = 'Add to Cart'
+            addToCart.className = "addBtn"
             addToCart.addEventListener('click', handleAddToCart)
             
             clothingCard.append(image, price, addToCart)
@@ -40,5 +39,11 @@ function renderClothing(){
 function handleAddToCart(){
     alert("Item Added to Cart!")
 
+    let cart = document.getElementById('cart')
+    let numberOfItems = document.createElement('p')
+    numberOfItems.innerText = //counter here 
 
+    cart.appendChild()
 }
+
+//add a clear all button to remove elements from dom that are currently there - all child nodes of the container
